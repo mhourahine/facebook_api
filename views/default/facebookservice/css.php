5d2a0cf4310ce7931e26964d585a20ffcae0ca4d
Pete Harris <dave@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-04-30 11:43:20 +0000

    Minor updates to external login services frontpage badges.

--- views/default/facebookservice/css.php
+++ views/default/facebookservice/css.php
@@ -1,7 +1,13 @@
+<?php
+/**
+ * Elgg Facebook Connect CSS
+ */
+?>
+
 #facebookservice_site_settings .text_input {
 	width: 350px;
 }
 
-#facebook_conenct {
-	padding: 10px 10px 10px 10px;
+#facebook_connect {
+	padding: 10px 0 0 0;
 }
--- views/default/facebookservice/login.php
+++ views/default/facebookservice/login.php
@@ -6,7 +6,7 @@
 $facebook_link = facebookservice_get_authorize_url();
 
 $login = <<<__HTML
-<div id="facebook_conenct">
+<div id="facebook_connect">
 	<a href="$facebook_link">
 		<img src="{$vars['url']}mod/facebookservice/graphics/login-button.png" />
 	</a>
